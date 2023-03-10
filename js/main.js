083b369a8a23336eeecdc30a8dbbfb410d964440
danilushka13 <dediaev.danil130702@gmail.com>
2023-03-10 18:28:06 +0300

    and again

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 //идея реализации взята с сайта https://learn.javascript.ru/task/random-int-min-max
 const getRandomNumber = function(a, b) {
   if (a < 0 || b < 0 || b <= a) {
-    console.log("Error: Incorrect input data");
+    //console.log('Error: Incorrect input data');
     return;
   }
   return Math.floor(a + Math.random() * (b + 1 - a));
-}
+};
 
-const checkLength = (currentStr, maxLength) => {
-  return currentStr.length <= maxLength ? true : false;
-}
+const checkLength = (currentStr, maxLength) => currentStr.length <= maxLength;
 
 checkLength('hello', 3);
 getRandomNumber(0, 5);
